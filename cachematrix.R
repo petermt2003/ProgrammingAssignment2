4022e64e444bf19510a65f76844cba7449aa25d8
petermt2003 <petermt2003@yahoo.co.uk>
2015-07-26 20:00:21 +0100

    version 2 of Prog. Assign. 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
 ## The result of the two functions is to return the inverse
 ## of a square matrix and then cache the result for future use.
 
-## This 
+
+## Creates a matrix object to act as cache for the inverse,
+## also creates list of parts of function which: set the matrix,
+## get the matrix; set the value of the inverse matrix; get the
+## value of the inverse matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -18,7 +22,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Checks whether an inverse matrix has already been calculated by
+## makeCacheMatrix, and if it has returns this; otherwise it
+## calculates the value of the inverse matrix and returns this
+## instead.
 
 cacheSolve <- function(x, ...) {
   m <- x$getsolve()
